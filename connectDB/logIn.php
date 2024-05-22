9ca754ee13228550d39dc50b3a9c4fd35ac236ae
SanneCodes <143067226+SanneCodes@users.noreply.github.com>
2024-05-22 09:23:50 +0200

    added echo 'test';

--- connectDB/logIn.php
+++ connectDB/logIn.php
@@ -21,17 +21,19 @@ echo "Connected successfully";
 $mail = $_POST['mailInput'] ?? null;
 $username = $_POST['usernameInput'] ?? null;
 $password = $_POST['passwordInput'] ?? null;
+echo "test1";
 
 if(!$username || !$password){
 	echo"All fields need to be completed";
 	exit;
 }
-
+echo "test2";
 $uCheck = "SELECT username FROM user WHERE username = ?";
 $uStmt = $conn->prepare($uCheck);
 $uStmt->bind_param("s", $username);
 $uStmt->execute();
 $uResult = $uStmt->get_result();
+echo "test3";
 
 if($uResult->num_rows == 0){
 	echo "Username not exist";
@@ -40,12 +42,14 @@ if($uResult->num_rows == 0){
 	exit;
 }
 $uStmt->close();
+echo "test3";
 
 $sql = "SELECT userId, mail, password FROM user WHERE username = ?";
 $stmt = $conn->prepare($sql);
 $query->bind_param("s", $username);
 $query->execute();
 $result = $query->get_result();
+echo "test4";
 
 $user = $result->fetch_assoc();
 if(!$user || !password_verify($password, $user['password'])){
@@ -54,9 +58,11 @@ if(!$user || !password_verify($password, $user['password'])){
 	$conn->close();
 	exit;
 }
+echo "test5";
 
 session_start();
 $_SESSION["id"] = $user["id"];
+echo "test6";
 
 $query->close();
 $conn->close();
