2b47d32a3c1e9a8da494975cd964bae0e4e763d9
SanneCodes <143067226+SanneCodes@users.noreply.github.com>
2024-05-23 08:58:27 +0200

    added more website files

--- html/mainpage.php
+++ html/mainpage.php
@@ -13,9 +13,29 @@ if (!isset($_SESSION["userId"])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>main</title>
+    <title>Home</title>
 </head>
 <body>
-    YAYYY MAIN PAGE LOGIN!
+    <nav>
+        <ul>
+            <span><li><a href="mainpage.php">LOGO!</a></li></span>
+
+            <li><a href="about.html">About</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li><a href="projects.html">Projects</a></li>
+            <li><a href="mainpage.php">Home</a></li>
+        </ul>
+    </nav>
+
+    <h1>gallery</h1>
+
+    <div class="gallery">
+        <a href="http://192.168.1.51" target="_blank">
+            <img src="../media/ArtJurney.png" alt="image of ArtJurney game" width="500" height="auto">
+        </a>
+        <div class="desc">Art Jurney</div>
+    </div>
+
 </body>
+<link rel="stylesheet" href="../css/home.css">
 </html>
