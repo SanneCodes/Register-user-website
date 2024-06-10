176b9a063233c1a4f477f21f09afc709930fbcc3
SanneCodes <codessand@gmail.com>
2024-06-11 01:20:08 +0200

    no password safe, but has to be min 8

--- connectDB/signUp.php
+++ connectDB/signUp.php
@@ -31,49 +31,26 @@ if($uResult->num_rows != 0){
 	echo "username allready exist";
 }
 else{
-	function specialChar($password){
-		return preg_match('/\W/', $password) === 1;
-	}
-
-	function capitalChar($password){
-		return preg_match('/[A-Z]/', $password) === 1;
-	}
-
-	function numChar($password){
-		return preg_match('/\d/', $password) === 1;
-	}
-
-	function whiteSpace($password){
-		return preg_match('/\s/', $password) === 0;
-	}
 
 	if (strlen($password)<8){
 		echo '<script>alert("Password needs to be atleast 8 characters long.")</script>';
 		header("Location: ../index.html")
 	}
 	else{
-		if (specialChar($password) && capitalChar($password) && numChar($password) && whiteSpace($password)){
-			$passwordHashed = password_hash($password, PASSWORD_BCRYPT);
-	
-			//insert bruker i db
-			$sql = "INSERT INTO user (mail, username, password) VALUES ('$mail', '$username', '$passwordHashed')";
-			//SQL-spørring for å sette inn brukerdata i 'User'-tabellen i databasen
-			//Utfører spørringen
-			if ($conn->query($sql) === TRUE){
-				//hvis insetting velykket, så viser suksessmelding
-				echo "inserted into database";
-				header("Location: ../html/login.html");
-				exit;
-			} else {
-				//hvis ikke velykket så viser error-melding
-				echo "error:" . $sql. "<br>" .$conn->error;
-			}
+		$passwordHashed = password_hash($password, PASSWORD_BCRYPT);
+
+		//insert bruker i db
+		$sql = "INSERT INTO user (mail, username, password) VALUES ('$mail', '$username', '$passwordHashed')";
+		//SQL-spørring for å sette inn brukerdata i 'User'-tabellen i databasen
+		//Utfører spørringen
+		if ($conn->query($sql) === TRUE){
+			//hvis insetting velykket, så viser suksessmelding
+			echo "inserted into database";
+		} else {
+			//hvis ikke velykket så viser error-melding
+			echo "error:" . $sql. "<br>" .$conn->error;
 		}
-		else{
-			echo '<script>alert("Your password must include a capital letter, a number and special characters")</script>';
-			header("Location: ../index.html");
-			exit;
-		}
+		header("Location: ../html/login.html");
 	}
 }
 
