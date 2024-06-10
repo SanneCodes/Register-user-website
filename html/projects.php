3448f3ed3321861f59ae4d3657a77c4f5d40055d
SanneCodes <143067226+SanneCodes@users.noreply.github.com>
2024-06-10 14:40:39 +0200

    fixed style changes

--- html/projects.php
+++ html/projects.php
@@ -6,7 +6,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My projects</title>
+    
     <link rel="stylesheet" href="../css/gallery.css">
+    <link rel="stylesheet" href="../css/nav.css">
 </head>
 <body>
     <nav>
